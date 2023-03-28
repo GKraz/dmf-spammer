ca142b51bdbcf32d11ebdd8a8ef9968f9cb85f9a
DMFGames <gkraz17@gmail.com>
2023-03-28 19:47:43 -0400

    thing

--- button.js
+++ button.js
@@ -1,27 +1,7 @@
-const button = document.getElementById("WebhookButton")
-let request
-let quantity
+const HtmlButton = document.getElementById("WebhookButton")
 
-const params = {
-    username: "I HATE DMF!!!",
-    content: "<@395361681087266816>\nVISIT: https://gkraz.github.io/",
-}
-
-button.onclick = () => {
-    quantity = document.getElementById("SpamQuantity").value
-
-    if (quantity == 0)
-        quantity = 1
-
-    for (let i = 0; i < quantity; i++)
-    {
-        request = new XMLHttpRequest()
-
-        request.open("POST", "https://discord.com/api/webhooks/1070562513491591238/tsaEmQfP55jB4WtULJxF-s4eX_SH9m6AYj51lZ6ZWwigdesgf0e6MzVmwHqlDkev3F5u")
-        request.setRequestHeader("Content-type", "application/json")
-
-        request.send(JSON.stringify(params))
-
-        delete button.request
-    }
+HtmlButton.onclick = () => {
+    let request = new XMLHttpRequest()
+    request.open("POST", "https://webhook-api-app-pal65.ondigitalocean.app/webhook")
+    request.send()
 }
